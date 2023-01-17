3371e3c2cf9f61e1c3c3ce394d60a1affde3fda2
Jade Giacoppo <jade@amaze.co>
2023-01-17 13:52:25 +1100

    Run script working for cli

--- app/prime_generator.rb
+++ app/prime_generator.rb
@@ -1,5 +1,4 @@
 class PrimeGenerator
-
    # Returns the updated primes array. Return can be ignored and is only
    # included for testing purposes (currently).
    def loadPrimes()
@@ -10,12 +9,16 @@ class PrimeGenerator
       return @primes
    end
 
-   def generate(n, primes)
-      n = floor(n)
-      primes = 2  #first prime
-
-      for i in 2..n
-         # if prime(i)
+   def generate(n)
+      if (!(n.to_i.eql? n) || n < 2)
+         raise "N must be an integer >= 2, but found #{n}"
       end
+      primes = [2]  #first prime
+      if (n > 2)
+         for i in 2..n
+            # if prime(i)
+         end
+      end
+      return primes
    end
 end
