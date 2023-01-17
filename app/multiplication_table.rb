8b3779038b922e3c7229a7e29110d13c4d08a068
Jade Giacoppo <jade@amaze.co>
2023-01-17 13:31:11 +1100

    Refactor multiplication table to print instead of output lists

--- app/multiplication_table.rb
+++ app/multiplication_table.rb
@@ -1,13 +1,61 @@
 class MultiplicationTable
    def initialize(table_numbers)
+      # Cannot remove sort from this line without updating invalid input checker in #generateTable.
       @numbers = [*table_numbers].sort
    end
 
-   def outputTable()
+   def generateTable()
+      checkInput()
+      return buildTableString()
+   end
+
+   private
+
+   def checkInput()
       # Relies on sort when assigning to instance variable.
       if(@numbers[0] < 2)
          raise "Invalid input, not a prime number: #{@numbers[0]}"
       end
+   end
+
+   def buildTableString()
+      @cell_size = cellSize()
+      table = "|" + " "* @cell_size + "|#{@numbers.map { |n| printNumberWithWhitespace(n)}.join("|")}|"
+      for row_prime in @numbers do
+         # add new row to multiplication table
+         table += "\n|#{printNumberWithWhitespace(row_prime)}|"
+         prime_index = 0
+         while prime_index < @numbers.length 
+            # add new column/cell
+            cell_value = row_prime * @numbers[prime_index]
+            table += "#{printNumberWithWhitespace(cell_value)}|"
+            prime_index = prime_index + 1
+         end
+      end
+      return table
+   end
+
+   def digitsInNumber(num)
+      if (num <1)
+         raise "Invalid number to calculate digits: #{num}. Number must be >1"
+      end
+      return Math.log10(num).floor() + 1
+   end
+
+   def cellSize()
+      largest_int = (@numbers.last)**2
+      num_digits_in_largest_int = digitsInNumber(largest_int)
+      return num_digits_in_largest_int + 3 #whitespace on either side, 2 in front, one behind
+   end
+
+   def printNumberWithWhitespace(number)
+      if(!defined?(@cell_size))
+         raise "Cannot generate number to print before cell_size is defined."
+      end
+
+      digits_in_number = digitsInNumber(number)
+      append_whitespace = @cell_size - digits_in_number - 1
+      return " " * append_whitespace + "#{number}" + " "
    end
 end
 
--- spec/unit_spec.rb
+++ spec/unit_spec.rb
@@ -2,27 +2,44 @@ require_relative '../app/multiplication_table.rb'
 require_relative '../app/prime_generator.rb'
 
 describe MultiplicationTable do
-	describe ".print output" do
+	describe ".generate table" do
     context "given invalid input" do
       it "returns error for [0]" do
         table = MultiplicationTable.new(0)
-        expect{ table.outputTable() }.to raise_error("Invalid input, not a prime number: 0")
+        expect{ table.generateTable() }.to raise_error("Invalid input, not a prime number: 0")
       end
       it "returns error for [1]" do
         table = MultiplicationTable.new(1)
-        expect{ table.outputTable() }.to raise_error("Invalid input, not a prime number: 1")
+        expect{ table.generateTable() }.to raise_error("Invalid input, not a prime number: 1")
       end
       it "returns error for [1, 2]" do
         table = MultiplicationTable.new([1,2])
-        expect{ table.outputTable() }.to raise_error("Invalid input, not a prime number: 1")
+        expect{ table.generateTable() }.to raise_error("Invalid input, not a prime number: 1")
       end
       it "returns error for [2, 1]" do
         table = MultiplicationTable.new([2,1])
-        expect{ table.outputTable() }.to raise_error("Invalid input, not a prime number: 1")
+        expect{ table.generateTable() }.to raise_error("Invalid input, not a prime number: 1")
       end
       it "returns error for [-2]" do
         table = MultiplicationTable.new(-2)
-        expect{ table.outputTable() }.to raise_error("Invalid input, not a prime number: -2")
+        expect{ table.generateTable() }.to raise_error("Invalid input, not a prime number: -2")
+      end
+    end
+
+    context "given valid input" do
+      it "generates a valid table for single input as integer" do
+        table = MultiplicationTable.new(2)
+        expect(table.generateTable()).to eq("|    |  2 |\n|  2 |  4 |")
+      end
+
+      it "generates a valid table for single input as list" do
+        table = MultiplicationTable.new([2])
+        expect(table.generateTable()).to eq("|    |  2 |\n|  2 |  4 |")
+      end
+
+      it "generates a valid table for multiple input" do
+        table = MultiplicationTable.new([2, 3, 5, 7])
+        expect(table.generateTable()).to eq("|     |   2 |   3 |   5 |   7 |\n|   2 |   4 |   6 |  10 |  14 |\n|   3 |   6 |   9 |  15 |  21 |\n|   5 |  10 |  15 |  25 |  35 |\n|   7 |  14 |  21 |  35 |  49 |")
       end
     end
   end
